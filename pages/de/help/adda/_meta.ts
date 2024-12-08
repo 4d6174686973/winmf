ec4f8a0d851e136e839b352185bc36bfb34cb7de
milovski69 <milo.makarski@gmail.com>
2024-12-08 16:30:17 +0000

    de feat lab seite angefangen

--- pages/de/help/adda/_meta.ts
+++ pages/de/help/adda/_meta.ts
@@ -1,4 +1,7 @@
 export default{
     'unterstuetzte-hardware': 'Unterstützte Hardware',
+    'frequency-response': 'Frequency response',
+    'impedance': 'Impedance',
+    'ls-sensitivity': 'LS sensitivity',
     
 }
