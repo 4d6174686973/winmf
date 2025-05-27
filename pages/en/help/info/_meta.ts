ca8aaf44d302a1805d80062fbd2750a632b8f525
Mathis Makarski <mathis.makarski@menschcologne.com>
2025-05-27 22:53:43 +0000

    level 3: info

--- pages/en/help/info/_meta.ts
+++ pages/en/help/info/_meta.ts
@@ -1,5 +1,9 @@
 export default {
-    statistics: 'Statistics',
-    'sti-impuse-response-und-stipa': 'STI (Impulse response and STIPA)',
-  }
-  
+  'statistics': 'Statistics',
+  'distortion': 'Distortion',
+  'sti-impuse-response-und-stipa': 'STI (Impulse response and STIPA)',
+  'decay-times': 'Decay times',
+  'loudspeaker-parameters': 'Loudspeaker parameters',
+  'room-acoustics': 'Room acoustics',
+  'room-acoustics-preprocessing': 'Room acoustics preprocessing',
+}
