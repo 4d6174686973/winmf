860e1605bb16c3e5d3595d2da213aba2185e35af
Mathis Makarski <mathis.makarski@menschcologne.com>
2025-05-31 20:08:15 +0200

    transform finished

--- pages/en/help/transform/_meta.ts
+++ pages/en/help/transform/_meta.ts
@@ -1,4 +1,8 @@
 export default {
+  'spectral-estimation': 'Spectral Estimation',
+  'fft': 'FFT',
+  'power-spectral-density': 'Power Spectral Density',
+  'ifft':'IFFT',
   'complex-mtf-of-impulse-response': 'Complex MTF of impulse response',
   'complex-mtf-of-stationary-signal-stipa-exciter': 'Complex MTF of stationary signal STIPA exciter',
 }
