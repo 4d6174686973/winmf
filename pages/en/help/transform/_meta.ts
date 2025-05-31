b18133b8f65706fd400776833a92cbf7d0db7729
Mathis Makarski <mathis.makarski@menschcologne.com>
2025-05-31 20:02:55 +0200

    help/info finished

---
+++ pages/en/help/transform/_meta.ts
@@ -0,0 +1,4 @@
+export default {
+  'complex-mtf-of-impulse-response': 'Complex MTF of impulse response',
+  'complex-mtf-of-stationary-signal-stipa-exciter': 'Complex MTF of stationary signal STIPA exciter',
+}
